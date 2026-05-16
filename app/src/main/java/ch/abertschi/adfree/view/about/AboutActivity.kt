2ac9dab0c4372aeb4124ff368d7d79d332971bc5
Alejandro <aleand136@gmail.com>
2026-05-16 18:02:13 +0200

    refactor: fix package directive, remove unnecessary safe calls and resolve Html.fromHtml deprecation in AboutActivity

--- app/src/main/java/ch/abertschi/adfree/view/about/AboutActivity.kt
+++ app/src/main/java/ch/abertschi/adfree/view/about/AboutActivity.kt
@@ -4,12 +4,13 @@
  * See the file "LICENSE" for the full license governing this code.
  */
 
-package ch.abertschi.adfree.view.setting
+package ch.abertschi.adfree.view.about
 
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Typeface
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.Html
@@ -22,7 +23,6 @@ import ch.abertschi.adfree.R
 import ch.abertschi.adfree.di.AboutModul
 import ch.abertschi.adfree.presenter.AboutPresenter
 import ch.abertschi.adfree.view.ViewSettings
-import ch.abertschi.adfree.view.about.AboutView
 
 /**
  * Created by abertschi on 21.04.17.
@@ -34,23 +34,28 @@ class AboutActivity : Fragment(), AboutView {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.about_view, container, false)
+        return inflater.inflate(R.layout.about_view, container, false)
     }
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
         typeFace = ViewSettings.instance(this.context!!).typeFace
 
         presenter = AboutModul(this.activity!!, this).provideAboutPresenter()
 
-        val textView = view?.findViewById(R.id.authorTitle) as TextView
+        val textView = view.findViewById<TextView>(R.id.authorTitle)
         textView.typeface = typeFace
         val text =
             "built with much &lt;3 by <font color=#FFFFFF>abertschi</font>. " +
                     "get my latest hacks and follow me on twitter."
 
-        textView?.text = Html.fromHtml(text)
+        textView.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(text)
+        }
 
         view.findViewById<ImageView>(R.id.twitter).setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW,
@@ -67,6 +72,5 @@ class AboutActivity : Fragment(), AboutView {
         view.findViewById<ImageView>(R.id.more_settings).setOnClickListener {
             presenter.showMoreSettings()
         }
-
     }
 }
